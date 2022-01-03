c28a7b0b87248160860a13b36188f6f77d2dc0bb
aoitori-asd <59700405+aoitori-asd@users.noreply.github.com>
2022-01-03 16:52:37 -0500

    Added time (needs styling)

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,10 @@
 
 // On document ready
 $(function(){
+    // Format table
     $('#programs').DataTable({
         paging: false,
     });
+    // Get JST time
+    $("#time-box").html(moment().tz('Asia/Tokyo').format('MM/DD') + "<br>" + moment().tz('Asia/Tokyo').format('h:ma z') + "JST");
 });
